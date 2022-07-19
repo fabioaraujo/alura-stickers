7f5530538032691cff4f3cb81d7bb324ad6971c8
Fabio Araujo <fabioaraujods@gmail.com>
2022-07-19 02:10:37 -0300

    efetuando parse do response via regex

--- src/main/java/br/com/fabioaraujo/App.java
+++ src/main/java/br/com/fabioaraujo/App.java
@@ -6,17 +6,16 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args ) throws IOException, InterruptedException
-    {
-        System.out.println( "Hello World!" );
+public class App {
 
+    public static void main( String[] args ) throws IOException, InterruptedException{
         var url = "https://raw.githubusercontent.com/alexfelipe/imersao-java/json/top250.json";
         URI endereco = URI.create(url);
         HttpClient client = HttpClient.newHttpClient();
@@ -25,7 +24,15 @@ public class App
 
         String body = response.body();
 
-        System.out.println(body);
+        var parser = new JsonParser();
+        List<Map<String, String>> listaDeFilmes = parser.parse(body);
+
+        for (Map<String,String> filme : listaDeFilmes) {
+            System.out.println(filme.get("title"));
+            System.out.println(filme.get("image"));
+            System.out.println(filme.get("imDbRating"));
+            System.out.println();            
+        }
 
     }
 }
